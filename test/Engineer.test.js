5898d60c03788de6e6fa0f879dce30109733ff4f
Sam Randels <samrandels@gmail.com>
2020-01-27 16:44:33 -0500

    Refactor code to match test variable and refactor tests to correct file paths and constructor names.

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,19 @@
-const Engineer = require("../lib/Engineer");
+const employee = require("../lib/employees");
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new employee.Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new employee.Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new employee.Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
