945c695950b5a6be75f7a7c62d07ee527caf0834
Sam Randels <samrandels@gmail.com>
2020-01-28 16:58:43 -0500

    Convert manager template to js

---
+++ templates/manager.js
@@ -0,0 +1,19 @@
+`<div class="col-12">
+  <div class="card text-center">
+    <div class="card-header">
+      Project Manager
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><!-- Manager Name--></h5>
+      <p class="card-text">
+        <!-- Manager ID -->
+      </p>
+      <p class="card-text">
+        <!-- Manager email -->
+      </p>
+      <p class="card-text">
+      <!-- Office Number -->
+      </p>
+    </div>
+  </div>
+</div>`
