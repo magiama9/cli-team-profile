0ef36a1b0663b393ae568db35bbc8ed85817f44f
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-28 15:17:04 -0500

    Add recursive inquirer prompts

--- index.js
+++ index.js
@@ -2,75 +2,176 @@ const inquirer = require("inquirer");
 const employee = require("./lib/employees");
 
 let testEmployee = new employee.Manager("Sam", "001", "Bossman", "UVA");
-console.log(testEmployee.getRole());
 
-const infoGather1 = () => {
-  inquirer
-    .prompt([
-      {
-        // Engineers
-        type: "input",
-        message: "How many engineers are on your team?",
-        name: "engineers"
-      },
-      {
-        // Interns
-        type: "input",
-        message: "How many interns are on your team?",
-        name: "interns"
-      }
-    ])
-    .then(response => {
-      let engineerCount = parseInt(response.engineers);
-      let internCount = parseInt(response.interns);
-      let teamCount = [engineerCount, internCount];
-      getTeamInfo(teamCount);
-    });
-};
+// Array for storing engineer employees
+let engineerArray = [];
 
+// Array for storing interns
+let internArray = [];
 
-const getTeamInfo =  arr => {
-  for (let i = 0; i < arr[0]; i++) {
-    inquirer
-      .prompt([
-        {
-          // Employee Name
-          type: "input",
-          message: "What is this engineer's name?",
-          name: "name"
-        },
-        {
-          // Employee ID
-          type: "input",
-          message: "What is their employee ID",
-          name: "id"
-        },
-        {
-          // Employee ID
-          type: "input",
-          message: "What is their email?",
-          name: "email"
-        },
-        {
-          // Employee Github
-          type: "input",
-          message: "What is this engineer's github username?",
-          name: "github"
-        }
-      ])
-      .then(response => {
-        let name = response.name;
-        let id = response.id;
-        let email = response.email;
-        let github = response.github;
-        let engineer = new employee.Engineer(name, id, email, github);
-        teamArray.push(engineer);
-      });
+// Object to store manager
+let manager;
+
+// Initialization Question
+const initialQuestion = [
+  {
+    type: "confirm",
+    name: "boss",
+    message: "Are you the manager of this project?"
   }
-  console.log(teamArray);
+];
+// questions array called for manager
+const managerQuestions = [
+  {
+    // Manager Name
+    type: "input",
+    message: "What is your name?",
+    name: "name"
+  },
+  {
+    // Manager ID
+    type: "input",
+    message: "What is your employee ID",
+    name: "id"
+  },
+  {
+    // Manager Email
+    type: "input",
+    message: "What is your email?",
+    name: "email"
+  },
+  {
+    
+  }
+];
+
+// Questions array called for interns
+const internQuestions = [
+  {
+    // Intern Name
+    type: "input",
+    message: "What is this intern's name?",
+    name: "name"
+  },
+  {
+    // Intern ID
+    type: "input",
+    message: "What is their employee ID",
+    name: "id"
+  },
+  {
+    // Intern ID
+    type: "input",
+    message: "What is their email?",
+    name: "email"
+  },
+  {
+    // Intern School
+    type: "input",
+    message: "Where did this intern go to school?",
+    name: "school"
+  },
+  {
+    // Check to see if you want to add more
+    type: "confirm",
+    message: "Would you like to add another intern?",
+    name: "add"
+  }
+];
+
+const engineerQuestions = [
+  {
+    // Engineer Name
+    type: "input",
+    message: "What is this engineer's name?",
+    name: "name"
+  },
+  {
+    // Engineer ID
+    type: "input",
+    message: "What is their employee ID",
+    name: "id"
+  },
+  {
+    // Engineer ID
+    type: "input",
+    message: "What is their email?",
+    name: "email"
+  },
+  {
+    // Engineer Github
+    type: "input",
+    message: "What is this engineer's github username?",
+    name: "github"
+  },
+  {
+    // Check to see if you want to add more
+    type: "confirm",
+    message: "Would you like to add another engineer?",
+    name: "add"
+  }
+];
+
+const initialize = () => {
+  inquirer.prompt(initialQuestion).then(answers => {
+    if (answers.boss === true) {
+      addManager();
+    } else {
+      console.log("You're not a manager. Only managers can create new teams.");
+      return;
+    }
+  });
+};
+const addManager = () => {
+  inquirer.prompt(managerQuestions).then(answers => {
+    // Sets the new manager object using user input
+    manager = new employee.Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.office
+    );
+  });
 };
 
-const prompts = async () => {
-  let teamCount = await infoGather1();
-  getTeamInfo(teamCount);
+const addEngineers = () => {
+  inquirer.prompt(engineerQuestions).then(answers => {
+    // Creates a new engineer object using user input
+
+    let engineer = new employee.Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    );
+
+    // Pushes the new engineer object to the storage array
+    engineerArray.push(engineer);
+    if (answers.add === true) {
+      addEngineers();
+    } else {
+      console.log("You're done adding engineers. Let's add some interns!");
+      addInterns();
+    }
+  });
 };
+
+const addInterns = () => {
+  inquirer.prompt(internQuestions).then(answers => {
+    // Creates a new intern object using user input
+    let intern = new employee.Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+
+    // Pushes the new intern object to the storage array
+    internArray.push(intern);
+    if (answers.add === true) {
+      addInterns();
+    } else console.log("You're done adding interns!");
+  });
+};
+
+addEngineers();
