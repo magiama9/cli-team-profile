ee833a8b06d31b8ea0c8dab81fe079e6c5ee54e3
Sam Randels <samrandels@gmail.com>
2020-01-28 16:57:05 -0500

    Convert templates to js with template literals

---
+++ templates/engineer.js
@@ -0,0 +1,18 @@
+`<div class="col-6">
+  <div class="card text-center">
+    <div class="card-header">
+      Engineer
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><!-- Engineer Name--></h5>
+      <p class="card-text">
+        <!-- Engineer ID -->
+      </p>
+      <p class="card-text">
+        <!-- Engineer email -->
+      </p>
+      <!-- Link to Github -->
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>`
