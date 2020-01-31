9135294d188fc8da8415fe08b9eb428561a548b1
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-31 10:16:29 -0500

    Add and test manager file writing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require("inquirer");
 const employee = require("./lib/employees");
 const questions = require("./lib/questions");
+const output = require("./lib/output");
 // const htmlgen = require("./lib/htmlgenerator");
-
 let testEmployee = new employee.Manager("Sam", "001", "Bossman", "UVA");
+output(testEmployee);
 
 // Array for storing engineer employees
 let engineerArray = [];
